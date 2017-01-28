4a7953e16411c3bd77da97a7a2c546793dfb0e97
Erik Heckel <Erik Heckel>
2017-01-28 15:10:00 -0500

    ordinal scales

--- js/app.js
+++ js/app.js
@@ -1,12 +1,8 @@
 // document.body.innerHTML = `<h1>${d3.version}</h1>`;
-var quantizeScale = d3.scaleQuantize()
-	.domain([0,100])
-	.range(['red','green','white','purple','yellow']);
+var ordinalScale = d3.scaleOrdinal()
+	.domain(['cold','cool','nice','okay','warm'])
+	.range(['blue','lightblue','white','yellow','green']);
 
-console.log(quantizeScale(18));
-console.log(quantizeScale(30));
-console.log(quantizeScale(50));
-console.log(quantizeScale(90));
-console.log(quantizeScale(64));
-
-console.log(quantizeScale.invertExtent('white'));
+console.log(ordinalScale('cool'));
+console.log(ordinalScale('warm'));
+console.log(ordinalScale('okay'));
