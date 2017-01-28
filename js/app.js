a8f66dc31c4df5ee52bd6170b3329d0d5a870283
Erik Heckel <Erik Heckel>
2017-01-28 16:22:35 -0500

    chart work d3

--- js/app.js
+++ js/app.js
@@ -1,8 +1,17 @@
-// document.body.innerHTML = `<h1>${d3.version}</h1>`;
-var ordinalScale = d3.scaleOrdinal()
-	.domain(['cold','cool','nice','okay','warm'])
-	.range(['blue','lightblue','white','yellow','green']);
+var margin = {top: 0, right: 0, bottom: 50, left: 50}
 
-console.log(ordinalScale('cool'));
-console.log(ordinalScale('warm'));
-console.log(ordinalScale('okay'));
+var width = 425 - margin.left - margin.right;
+var height = 625 - margin.top - margin.bottom;
+
+var svg = d3.select('.chart')
+	.append('svg')
+		.attr('width', width + margin.left + margin.right)
+		.attr('height', height + margin.top + margin.bottom)
+	.append('g')
+		.attr('transform', `translate(${margin.left}, ${margin.top})`)
+
+svg.append('rect')
+	.attr('width', width)
+	.attr('height', height)
+	.style('fill', 'lightblue')
+	.style('stroke', 'navy')
